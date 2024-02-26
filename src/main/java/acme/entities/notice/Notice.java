6a5ca4e4f6b6ca898244566e1874944eda059920
Ronald752 <ronmonalb@alum.us.es>
2024-02-26 20:05:47 +0100

    feat: Add notice entity

---
+++ src/main/java/acme/entities/notice/Notice.java
@@ -0,0 +1,52 @@
+
+package acme.entities.notice;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
+
+import acme.client.data.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class Notice extends AbstractEntity {
+
+	private static final long	serialVersionUID	= 1L;
+
+	@Past
+	@Temporal(TemporalType.TIMESTAMP)
+	@NotNull
+	private Date				moment;
+
+	@NotBlank
+	@Length(max = 75)
+	private String				title;
+
+	@NotBlank
+	@Length(max = 75)
+	@Pattern(regexp = "^\\w+ - \\w+, \\w+$")
+	private String				author;
+
+	@NotBlank
+	@Length(max = 100)
+	private String				message;
+
+	@Email
+	private String				email;
+
+	@URL
+	private String				link;
+}
