ecc864b0c07771794eef2cb8fe9ee7be30af89c3
jaicabher1 <jaicabher1@alum.us.es>
2024-02-27 09:49:12 +0100

    feat: Add trainings entities

---
+++ src/main/java/acme/entities/training/TrainingModule.java
@@ -0,0 +1,47 @@
+
+package acme.entities.training;
+
+import java.sql.Date;
+
+import javax.persistence.Column;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
+
+import acme.client.data.AbstractEntity;
+
+public class TrainingModule extends AbstractEntity {
+
+	private static final long	serialVersionUID	= 1L;
+
+	@NotBlank
+	@Column(unique = true)
+	@Pattern(regexp = "[A-Z]{1,3}-[0-9]{3}")
+	private String				code;
+
+	@Past
+	@NotNull
+	private Date				creationMoment;
+
+	@NotBlank
+	@Length(max = 100)
+	private String				details;
+
+	private DifficultyLevel		difficultyLevel;
+
+	//After(creationMoment)
+	@Past
+	private Date				updateMoment;
+
+	@URL
+	private String				link;
+
+	@Min(0)
+	private double				totalTime;
+
+}
---
+++ src/main/java/acme/entities/training/TrainingSession.java
@@ -0,0 +1,48 @@
+
+package acme.entities.training;
+
+import java.sql.Date;
+
+import javax.persistence.Column;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
+
+import acme.client.data.AbstractEntity;
+
+public class TrainingSession extends AbstractEntity {
+
+	private static final long	serialVersionUID	= 1L;
+
+	@NotBlank
+	@Column(unique = true)
+	@Pattern(regexp = "TS-[A-Z]{1,3}-[0-9]{3}")
+	private String				code;
+
+	//Revisar
+	@Past
+	@NotNull
+	private Date				period;
+
+	@NotBlank
+	@Length(max = 75)
+	private String				location;
+
+	@NotBlank
+	@Length(max = 75)
+	private String				instructor;
+
+	@NotBlank
+	@NotNull
+	@Email
+	private String				email;
+
+	@URL
+	private String				link;
+
+}
