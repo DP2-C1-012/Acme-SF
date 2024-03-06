ffbd7221e38b0bed5688af69cd7c607b1055ac41
JuanJoCasamitjana <73534780+JuanJoCasamitjana@users.noreply.github.com>
2024-03-06 11:14:55 +0100

    fix: Task 045c
    Fixed Aduit Records constraints

--- src/main/java/acme/entities/auditRecord/AuditRecord.java
+++ src/main/java/acme/entities/auditRecord/AuditRecord.java
@@ -7,6 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
@@ -29,12 +30,17 @@ public class AuditRecord extends AbstractEntity {
 	@Pattern(regexp = "AU-[0-9]{4}-[0-9]{3}")
 	private String				code;
 	@Temporal(TemporalType.TIMESTAMP)
+	@NotNull
 	private Date				periodStart;
 	@Temporal(TemporalType.TIMESTAMP)
+	@NotNull
 	private Date				periodEnd;
+	@NotNull
 	private Mark				mark;
 	@URL
 	private String				optionalLink;
 	@ManyToOne(optional = false)
+	@NotNull
+	@Valid
 	private CodeAudit			codeAudit;
 }
