dcd40d75b26523493aa607ae0648a7b69e331685
[alvarobernal2412] <alvarobc2412@gmail.com>
2024-03-07 21:07:58 +0100

    feat: Handle sponsor dashboards

--- src/main/java/acme/forms/SponsorDashboard.java
+++ src/main/java/acme/forms/SponsorDashboard.java
@@ -1,41 +1,24 @@
 
 package acme.forms;
 
-import javax.persistence.Entity;
-import javax.validation.constraints.Max;
-
 import acme.client.data.AbstractForm;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-@Entity
 public class SponsorDashboard extends AbstractForm {
 
-	// Serialisation identifier -----------------------------------------------------
-	private static final long	serialVersionUID	= 1L;
+	protected static final long	serialVersionUID	= 1L;
 
-	// Attributes -----------------------------------------------------
-	@Max(21)
-	private Integer				invoices;
-
-	private Integer				invAverage;
-
-	private Integer				invDeviation;
-
-	private Integer				invMinimum;
-
-	private Integer				invMaximum;
-
-	private Integer				sponsorships;
-
-	private Integer				spAverage;
-
-	private Integer				spDeviation;
-
-	private Integer				spMinimum;
-
-	private Integer				spMaximum;
-
+	int							numberOfInvoicesWithATaskLessOrEqualThan21;
+	int							numberOfSponsorshipsWithALink;
+	double						averageAmountOfSponsorships;
+	double						deviationAmountOfSponsorships;
+	double						minimumAmountOfSponsorships;
+	double						maximumAmountOfSponsorships;
+	double						averageQuantityOfInvoices;
+	double						deviationQuantityOfInvoices;
+	double						minimumQuantityOfInvoicess;
+	double						maximumQuantityOfInvoices;
 }
