ab52c6b6f387b88d5d2cba1309aa40bf8d91b5c7
jaicabher1 <jaicabher1@alum.us.es>
2024-04-03 20:07:41 +0200

    feat: list and show training modules details

---
+++ src/main/java/acme/features/developer/trainingSession/DeveloperTrainingSessionController.java
@@ -0,0 +1,29 @@
+
+package acme.features.developer.trainingSession;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import acme.client.controllers.AbstractController;
+import acme.entities.trainingSession.TrainingSession;
+import acme.roles.Developer;
+
+@Controller
+public class DeveloperTrainingSessionController extends AbstractController<Developer, TrainingSession> {
+
+	@Autowired
+	protected DeveloperTrainingSessionListService	listTrainingSessions;
+
+	@Autowired
+	private DeveloperTrainingSessionShowService		showService;
+
+
+	@PostConstruct
+	protected void initialise() {
+		super.addBasicCommand("list", this.listTrainingSessions);
+		super.addBasicCommand("show", this.showService);
+	}
+
+}
---
+++ src/main/java/acme/features/developer/trainingSession/DeveloperTrainingSessionRepository.java
@@ -0,0 +1,24 @@
+
+package acme.features.developer.trainingSession;
+
+import java.util.Collection;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import acme.client.repositories.AbstractRepository;
+import acme.entities.trainingSession.TrainingSession;
+
+@Repository
+public interface DeveloperTrainingSessionRepository extends AbstractRepository {
+
+	@Query("select ts from TrainingSession ts where ts.id = :id")
+	TrainingSession findTrainingSessionById(int id);
+
+	@Query("select ts from TrainingSession ts")
+	Collection<TrainingSession> findAllTrainingSessions();
+
+	@Query("select ts from TrainingSession ts where ts.trainingModule.id = :id")
+	Collection<TrainingSession> findAllTrainingSessionsBytrainingModuleId(int id);
+
+}
