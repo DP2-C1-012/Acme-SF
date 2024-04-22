9ccb8ff6573babd88e1e147d11a3e647ee1b5772
JuanJoCasamitjana <73534780+JuanJoCasamitjana@users.noreply.github.com>
2024-04-22 09:11:53 +0200

    Task087 start

---
+++ src/main/java/acme/features/authenticated/codeAudit/AuthenticatedCodeAuditController.java
@@ -0,0 +1,9 @@
+
+package acme.features.authenticated.codeAudit;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class AuthenticatedCodeAuditController {
+
+}
---
+++ src/main/java/acme/features/authenticated/codeAudit/AuthenticatedCodeAuditListService.java
@@ -0,0 +1,6 @@
+package acme.features.authenticated.codeAudit;
+
+
+public class AuthenticatedCodeAuditListService {
+
+}
