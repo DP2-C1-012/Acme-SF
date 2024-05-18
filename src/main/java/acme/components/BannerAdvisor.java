024e9f9e0014cbafb551e498ef3f705472d6701a
JuanJoCasamitjana <73534780+JuanJoCasamitjana@users.noreply.github.com>
2024-05-18 12:41:58 +0200

    Corrección random

--- src/main/java/acme/components/BannerAdvisor.java
+++ src/main/java/acme/components/BannerAdvisor.java
@@ -4,13 +4,13 @@ package acme.components;
 import java.util.Collection;
 import java.util.Date;
 import java.util.List;
-import java.util.Random;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
 import acme.client.helpers.MomentHelper;
+import acme.client.helpers.RandomHelper;
 import acme.entities.banner.Banner;
 
 @ControllerAdvice
@@ -28,8 +28,7 @@ public class BannerAdvisor {
 			Collection<Banner> all = this.repository.findAllBanners();
 			List<Banner> baList;
 			baList = all.stream().unordered().filter(b -> (b.getDisplayPeriodStart().after(current) || b.getDisplayPeriodStart().equals(current) && (b.getDisplayPeriodEnd().before(current) || b.getDisplayPeriodEnd().equals(current)))).toList();
-			Random rand = new Random();
-			int index = rand.nextInt(baList.size());
+			int index = RandomHelper.nextInt(0, baList.size());
 			res = baList.get(index);
 		} catch (final Throwable oops) {
 			oops.printStackTrace();
